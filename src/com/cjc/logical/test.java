f9058d4a33c3e9b403f7d7e6c18be3a4b29d82dd
USER <USER@DESKTOP-7CMDVD0>
2021-05-05 01:04:07 +0530

    practical project

---
+++ src/com/cjc/logical/test.java
@@ -0,0 +1,80 @@
+package com.cjc.logical;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class test {
+//reverse string
+	public static void main(String[] args) {
+	/*{
+		String str;
+		String revstr=" ";
+		Scanner sc=new Scanner(System.in);
+		System.out.println("print string");
+		str=sc.nextLine();
+		int len=str.length();
+		for(int i= len-1; i>=0;i--)
+		revstr=revstr+str.charAt(i);
+		System.out.println(revstr);
+	}
+
+	{int num;
+	boolean flag=false;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("print no");
+		num=sc.nextInt();
+		for(int i=2;i<=num/2;i++)
+		{
+			if(num%i==0)
+			{
+				
+				if(flag==true)
+					break;
+		}
+		
+		
+		}	
+	if(!flag)
+	
+		System.out.println(num+"no is prime");
+		else
+			System.out.println(num +"no is not prime");}
+	
+	*/
+	
+	
+	
+/*	int array[]= {10,20,30};
+	int size=array.length;
+	Arrays.sort(array);
+	System.out.println("sorted array is:"+Arrays.toString(array));
+	int res=array[0];
+	System.out.println(res);
+	
+*/
+	
+	
+	/*String s[]="india is my contry".split(" ");
+	
+	String temp=" ";
+	for(int i=s.length-1;i>=0;i--)
+	{
+		temp+=s[i]+" ";
+		
+	}
+	System.out.println("reverse string:-----" +temp);
+	 System.out.print("\n");
+s
+	}
+		
+*/
+		
+	
+	
+	
+	
+	
+	}}	
+	
+	
